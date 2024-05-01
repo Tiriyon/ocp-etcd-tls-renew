ae4c09ba1d95943502290ee8f74fa4250e480fdc
tom <tiriyon@gmail.com>
2024-05-01 12:01:50 +0300

    TEST: OK get-signer.sh
    **Test results:**
    
    - Script retrieves root certificates to bastion
    - Script generates exported_vars.env file correctly
    
    FIX: var structure

--- get-signer.sh
+++ get-signer.sh
@@ -29,6 +29,6 @@ done
 
 # Exporting variables for use in etcd-cert-gen.sh script
 
-echo "etcd_signers_dir=\"$local_root_certs_dir\"" > exported_vars.env
+echo "$local_root_certs_dir" > exported_vars.env
 
 echo "Extraction and transfer process completed. Check $local_root_certs_dir for certificates and keys with validation script"
