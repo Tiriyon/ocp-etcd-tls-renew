1e6edb7f810d9d49e9f9a452a7459dbc395a2612
tom <tiriyon@gmail.com>
2024-05-01 14:39:07 +0300

    fix: local certs tar, cluster key

--- og-certs.sh
+++ og-certs.sh
@@ -4,11 +4,13 @@ ssh_user="core"
 nodes_env_file="nodes.env"
 etcd_secrets_dir="/etc/kubernetes/static-pod-resources/etcd-certs/secrets"
 og_certs_tar="/tmp/og-certs-$(date +%Y%m%d).tar"
+local_certs_tar="./og-certs-$(date +%Y%m%d).tar"
+cluster_key="./cluster-key"
 read -r master_hostname master_ip < "$nodes_env_file"
 
 ssh -i "./$cluster_key" -t "$ssh_user@$master_ip" "sudo bash -c '
-tar cvf /tmp/og-certs-$(date +%Y%m%d).tar -C $etcd_secrets_dir . 
+tar cvf /tmp/og-certs-$(date +%Y%m%d).tar -C $etcd_secrets_dir .
 '"
 
-scp -i "./$cluster_key" "$ssh_user@$master_ip:$og_certs_tar ." 
-tar xvf $og_certs_tar
+scp -i "./$cluster_key" "$ssh_user@$master_ip:$og_certs_tar" "."
+tar xvf $local_certs_tar
